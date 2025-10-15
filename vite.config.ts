1a6f4ed7a7a9922f5dc926730f527071ee04911d
TitanlordCode <titanlord@gmx.de>
2025-10-15 13:37:32 +0200

    setup(cleanup): add typescript support dev packages and others

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import dts from 'vite-plugin-dts'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    dts({
+      tsconfigPath: './tsconfig.app.json',
+      entryRoot: 'src',
+      outDir: 'dist',
+      include: ['src/**/*.ts', 'src/**/*.vue'],
+      exclude: ['src/**/*.stories.ts', 'src/**/__tests__/**'],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
